2fef70e17c21f5eff2c1ef4e70479eea4dc4ef10
aenesgur <47754791+aenesgur@users.noreply.github.com>
2019-12-30 11:28:11 +0300

    10 Fast Fingers App

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 10 Fast Finger App
  
+This application was developed with pure Javascript to help you type 10 fast fingers.
+We fetch the data from the local JSON file and data is written to the screen using DOM.
+Level buttons, result field and head section where we write current level information are also written on the screen using DOM.
+Our current number of true at the current level are saved in Local Storage and written in the "number of true" in the result field. 
+Local Storage is cleared when the page is refreshed and when we complete the current level.
+
+# How to run this project
+## If NPM is installed on your computer, follow these steps, otherwise install npm first, then follow the steps
+*After downloading the project, enter the project directory from the terminal screen, and then type the following commands
+>npm install
+
+>npm run-script build
+
+>npm start
+
+Then you can test the project at localhost: 8080
--- src/index.js
+++ src/index.js
@@ -2,68 +2,87 @@ let time = 1; // Time value of the timer function
 let counter = 0; // Counter of the current input
 let level=-1; // Exercise level of the JSON data , move getData function
 let currentTime; // variable of the timer function
-let resultPlace = ``; // resultField place , show with createResultField function and hide with click startButton
+let resultFieldOutput = ``; // resultField place , show with createResultField function and hide with click startButton
 let totalLevel = 0; // Keep total level
 let challengeData; // Current datas in json
 
-const challengeInput = document.getElementById('CI');
+const challengeInput = document.getElementById('challengeInputField');
 const inputText = document.getElementById('inputText');
-const startButton = document.getElementById('startButton');
-const mainpart = document.getElementById('mainPart');
+const nextButton = document.getElementById('nextButton');
+const mainpart = document.querySelectorAll('#mainPart');
 const gameInfo = document.getElementById('gameInfo');
 const againButton = document.getElementById('againButton');
-const resultField = document.getElementById('resultPlace');
-const spanValues = document.getElementById('spanValues');
-const timePlace = document.getElementById('timePlace');
+const resultField = document.getElementById('resultField');
+const challengeDataField = document.getElementById('challengeDataField');
+const timeField = document.getElementById('timeField');
 const levelButtons = document.getElementById('levelButtons');
 const levelInfo =  document.getElementById('levelInfo');
+const dropdownControl = document.getElementById('dropdownControl');
+const dropdownButton = document.getElementById('dropdownList');
+const startButton = document.getElementById('startButton');
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function() { 
     localStorage.removeItem('trues');
     getData();
   });
 
-document.querySelector('#startButton').addEventListener('click',function(event){
+startButton.addEventListener('click',function(){
+    for(let mainAttrLen=0;mainAttrLen<mainpart.length;mainAttrLen++){
+        mainpart[mainAttrLen].classList.add('setAttr');
+    }
     createLevelButtons(challengeData);
     challengeInput.classList.add('active');
-    startButton.classList.add('disabled');
-    mainpart.classList.add('setAttr');
+    dropdownControl.classList.add('active');
     gameInfo.classList.add('disabled');
-    levelButtons.classList.add('active');
     againButton.classList.remove('active'); //After press next button,this will be hidden
-    startButton.innerHTML='Next<i class="fas fa-chevron-right"></i>';
     resultField.innerHTML=``;
     level++;
     createField(challengeData,level);
     currentTime = setInterval(timer, 1000);
 });
 
-document.querySelector('#againButton').addEventListener('click',function(event){
+nextButton.addEventListener('click',function(){
+    for(let mainAttrLen=0;mainAttrLen<mainpart.length;mainAttrLen++){
+        mainpart[mainAttrLen].classList.add('setAttr');
+    }
+    createLevelButtons(challengeData);
+    challengeInput.classList.add('active');
+    nextButton.classList.remove('active');
+    dropdownControl.classList.add('active');
+    gameInfo.classList.add('disabled');
+    againButton.classList.remove('active'); //After press next button,this will be hidden
+    resultField.innerHTML=``;
+    level++;
+    createField(challengeData,level);
+    currentTime = setInterval(timer, 1000);
+});
+
+againButton.addEventListener('click',function(){
     challengeInput.classList.add('active');
     againButton.classList.remove('active');
-    startButton.classList.add('disabled');
-    levelButtons.classList.add('active');
+    nextButton.classList.remove('active');
+    dropdownControl.classList.add('active');
     resultField.innerHTML=``;
     createField(challengeData,level);
     currentTime = setInterval(timer, 1000);
 });
 
-document.querySelector('#inputText').addEventListener('input',function(event){
+inputText.addEventListener('input',function(event){
     let keyValue = event.data;
-    let currentSpanValue = spanValues.childNodes[counter].innerHTML;
+    let currentSpanValue = challengeDataField.childNodes[counter].innerHTML;
     disabledBackspace();
 
-    let lastValueCheck = spanValues.childNodes[counter+1];
+    let lastValueCheck = challengeDataField.childNodes[counter+1];
     if(lastValueCheck!=undefined){
-        spanValues.childNodes[counter+1].classList.add('current');
+        challengeDataField.childNodes[counter+1].classList.add('current');
         compareValues(keyValue,currentSpanValue);  
-        spanValues.childNodes[counter].classList.remove('current');
+        challengeDataField.childNodes[counter].classList.remove('current');
         counter++;
         
     }
     else{
         compareValues(keyValue,currentSpanValue);
-        spanValues.childNodes[counter].classList.remove('current');
+        challengeDataField.childNodes[counter].classList.remove('current');
         resultOfChallenge();
         counter=0;
     }
@@ -71,18 +90,18 @@ document.querySelector('#inputText').addEventListener('input',function(event){
 });
 
 function timer() {
-  timePlace.innerHTML ='<i class="far fa-clock"></i>  '+time+'sec';
-  time++;
+    timeField.innerHTML ='<i class="far fa-clock"></i>  '+time+'sec';
+    time++;
 }
 
 function resultOfChallenge(){
     clearInterval(currentTime);
+    dropdownControl.classList.remove('active');
     challengeInput.classList.remove('active');
     if(level!=totalLevel){
-        startButton.classList.remove('disabled');
+        nextButton.classList.add('active');
     }
     againButton.classList.add('active');
-    levelButtons.classList.remove('active');
     inputText.value="";
     let result= JSON.parse(localStorage.getItem('trues'));
     
@@ -95,25 +114,39 @@ function resultOfChallenge(){
 }
 
 function createResultField(resultLength,time){
-    let score = resultLength/(time-1);
-    resultPlace = `<div class="alert alert-warning" role="alert">
-                     <p class="result"> Number of true: ${resultLength} </p>
-                     <p class="result"> Time: ${time-1} sec </p>
-                     <p class="result"> Score: ${score}(represent) </p>
-                    </div>`;
+    let score = Math.ceil(resultLength/(time-1));
+    let emoji;
+    if(score>=7){
+        emoji = "grin-stars";
+    }
+    else if(score >= 5){
+        emoji = "smile";
+    }
+    else if(score >= 1){
+        emoji = "flushed";
+    }
+    else{
+        emoji = "sad-cry";
+    }
+    resultFieldOutput = `<div class="alert alert-warning" role="alert">
+                            <p class="result"> <b>Number of true(s):</b> ${resultLength} </p>
+                            <p class="result"> <b>Time:</b> ${time-1} sec </p>
+                            <p class="result"> <b>Score:</b> ${score}(represent) </p>
+                            <p class="result"> <i class="far fa-${emoji} fa-2x"></i>  </p>
+                        </div>`;
     
-    resultField.innerHTML=resultPlace;
+    resultField.innerHTML=resultFieldOutput;
 }
 
 function compareValues(keyValue,currentSpanValue){
     if(keyValue==currentSpanValue){
-        spanValues.childNodes[counter].classList.add('true');
+        challengeDataField.childNodes[counter].classList.add('true');
         const trueSound=new Audio('sounds/true.mp3');
         trueSound.play();
         setLocalStorage();
     }
     else{
-        spanValues.childNodes[counter].classList.add('false');
+        challengeDataField.childNodes[counter].classList.add('false');
         const falseSound=new Audio('sounds/false.mp3');
         falseSound.play();
     }
@@ -146,7 +179,7 @@ function createField(data,level){
     for (let i = 0; i < exercise.length; i++) {
       output +=`<div class="valuesCard text-center">${exercise[i]}</div>`;
     }
-    spanValues.innerHTML=output;
+    challengeDataField.innerHTML=output;
     totalLevel=data.length-1;
     controlLevelField(level);
 }
@@ -163,15 +196,15 @@ function controlLevelField(level) {
   function createLevelButtons(data){
       let levelFieldOutput=``;
       for(let levelCounter = 0; levelCounter<data.length; levelCounter++){
-          levelFieldOutput+=` <button id="${levelCounter+1}" type="button" class="list-group-item list-group-item-action ">Level ${levelCounter+1}</button>`;
-      }
-      levelButtons.innerHTML=levelFieldOutput;
+        levelFieldOutput+=`<button id="${levelCounter+1}" class="levelBtn levelBtnControl">Level ${levelCounter+1}</button>`;        
+        }
+      dropdownButton.innerHTML=levelFieldOutput;
       controlLevelButtons();
   }
 
   function controlLevelButtons(){
-    for(let currentButton = 0 ; currentButton<levelButtons.children.length ; currentButton++){
-        levelButtons.children[currentButton].addEventListener('click',function(){
+    for(let currentButton = 0 ; currentButton<dropdownButton.children.length ; currentButton++){
+        dropdownButton.children[currentButton].addEventListener('click',function(){
             let levelId = this.id;
             level=levelId-1;
             createField(challengeData,level);
